fb576106386cef80ff9ab4d51847a4919e10fe8d
Yusuf Aktan <yusufaktn@outlook.com>
2024-10-16 21:45:13 +0300

    init

---
+++ src/main/java/com/aktanyusuf/shipment_tracking/ShipmentTrackingApplication.java
@@ -0,0 +1,13 @@
+package com.aktanyusuf.shipment_tracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShipmentTrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShipmentTrackingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aktanyusuf/shipment_tracking/ShipmentTrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aktanyusuf.shipment_tracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShipmentTrackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
